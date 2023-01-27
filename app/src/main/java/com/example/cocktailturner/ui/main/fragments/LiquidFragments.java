eb24e9baac821e644c3fc837f1aeecaace087d14
Eren Dogan <eren51@live.at>
2023-01-27 20:02:46 +0100

    added liquid fragment

---
+++ app/src/main/java/com/example/cocktailturner/ui/main/fragments/LiquidFragments.java
@@ -0,0 +1,18 @@
+package com.example.cocktailturner.ui.main.fragments;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class LiquidFragments extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+}
