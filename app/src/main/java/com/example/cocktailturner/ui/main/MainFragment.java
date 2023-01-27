70d9cff52feea59b4252d33164a413c06da2509a
goldis <goldis.salarmaaf@hotmail.com>
2023-01-27 19:20:06 +0100

    test

--- app/src/main/java/com/example/cocktailturner/ui/main/MainFragment.java
+++ app/src/main/java/com/example/cocktailturner/ui/main/MainFragment.java
@@ -8,11 +8,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.cocktailturner.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainFragment extends Fragment {
 
@@ -26,6 +30,7 @@ public class MainFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        mViewModel.setName("Test");
         // TODO: Use the ViewModel
     }
 
@@ -34,6 +39,23 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextInputEditText nameEditText = view.findViewById(R.id.textInputTextName);
+        TextView showText = view.findViewById(R.id.showText);
+        // Add Text Watcher on name input text
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mViewModel.setName(charSequence.toString());
+                showText.setText(charSequence.toString());
+            }
+            @Override public void afterTextChanged(Editable editable) {
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/cocktailturner/ui/main/MainViewModel.java
+++ app/src/main/java/com/example/cocktailturner/ui/main/MainViewModel.java
@@ -1,7 +1,17 @@
 package com.example.cocktailturner.ui.main;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+
+    private MutableLiveData<String> mName = new MutableLiveData<>();
+    public void setName(String name) {
+        mName.setValue(name);
+    }
+    public LiveData<String> getName() {
+        return mName;
+    }
 }
